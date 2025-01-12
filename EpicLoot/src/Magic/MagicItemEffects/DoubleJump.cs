905b7bbb1e7b891a78040b0a93b0f6d18b273ed7
MidnightsFX <carl.stutz@gmail.com>
2025-01-11 20:13:33 -0800

    multijump math fix and support for multi-multi jumps

--- EpicLoot/src/Magic/MagicItemEffects/DoubleJump.cs
+++ EpicLoot/src/Magic/MagicItemEffects/DoubleJump.cs
@@ -27,11 +27,15 @@ namespace EpicLoot.MagicItemEffects
                         multi_jump_combo++;
                     }
                     // You can't keep jumping
-                    if (multi_jump_combo >= Player.m_localPlayer.GetTotalActiveMagicEffectValue(MagicEffectType.DoubleJump))
+                    // EpicLoot.Log($"multi_jump_combo: {multi_jump_combo} > {Player.m_localPlayer.GetTotalActiveMagicEffectValue(MagicEffectType.DoubleJump)}");
+                    if (multi_jump_combo > Player.m_localPlayer.GetTotalActiveMagicEffectValue(MagicEffectType.DoubleJump))
                     {
                         return false;
                     } else {
-                        MultiJump(__instance);
+                        // We can jump, our current jump combo is zero, the first jump is a normal one.
+                        if (multi_jump_combo == 0) { return true; }
+                        // We can jump, our current jump combo is greater than zero, we can multi-jump.
+                        MultiJump(__instance, multi_jump_combo);
                         // skip the original jump, since we already multi-jumped
                         return false;
                     }
@@ -41,7 +45,7 @@ namespace EpicLoot.MagicItemEffects
             }
         }
 
-        public static void MultiJump(Character player)
+        public static void MultiJump(Character player, float jumpsize)
         {
             if (player.IsEncumbered() || player.InDodge() || player.IsKnockedBack() || player.IsStaggering())
             {
@@ -65,16 +69,13 @@ namespace EpicLoot.MagicItemEffects
             }
 
             Vector3 jump = player.m_body.velocity;
-            Vector3 player_vector_up = (player.gameObject.transform.position + Vector3.up).normalized;
+            // We just want to go upwards, the jump value will keep some of our other velocities
+            Vector3 player_vector_up = (new Vector3() + Vector3.up).normalized;
+            player_vector_up.y += (0.2f * jumpsize); // this increases velocity upwards slightly for each multi-jump
             float player_skill_jump_with_offset = 1f + player_jum_skill_factor * 0.4f;
             float full_player_jumpforce = player.m_jumpForce * player_skill_jump_with_offset;
-            float player_normal_direction_velocity_travel = Vector3.Dot(player_vector_up, jump);
-            if (player_normal_direction_velocity_travel < full_player_jumpforce)
-            {
-                jump += player_vector_up * (full_player_jumpforce - player_normal_direction_velocity_travel);
-            }
-
-            jump += player.m_moveDir * player.m_jumpForceForward * player_skill_jump_with_offset;
+            // float player_normal_direction_velocity_travel = Vector3.Dot(player_vector_up, jump);
+            jump += player_vector_up * full_player_jumpforce;
 
             player.m_seman.ApplyStatusEffectJumpMods(ref jump);
             if (!(jump.x <= 0f) || !(jump.y <= 0f) || !(jump.z <= 0f))
