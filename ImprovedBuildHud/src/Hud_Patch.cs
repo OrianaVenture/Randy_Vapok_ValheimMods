7ac547e36f229ea5a0f87dbdf830db9045037450
OrianaVenture <107574865+OrianaVenture@users.noreply.github.com>
2025-03-10 09:55:38 -0500

    Improved Build Hud Beta for 1.0.9:
    * Reworked patches to be less invasive.

--- ImprovedBuildHud/src/Hud_Patch.cs
+++ ImprovedBuildHud/src/Hud_Patch.cs
@@ -1,40 +1,40 @@
 ﻿using HarmonyLib;
 using TMPro;
 
-namespace ImprovedBuildHud
+namespace ImprovedBuildHud;
+
+[HarmonyPatch(typeof(Hud), nameof(Hud.SetupPieceInfo), typeof(Piece))]
+public static class Hud_Patch
 {
-    [HarmonyPatch(typeof(Hud), nameof(Hud.SetupPieceInfo), typeof(Piece))]
-    public static class Hud_Patch
+    private static void Postfix(Piece piece, TMP_Text ___m_buildSelection)
     {
-        private static void Postfix(Piece piece, TMP_Text ___m_buildSelection)
+        if (piece != null && !string.IsNullOrEmpty(ImprovedBuildHudConfig.CanBuildAmountFormat.Value))
         {
-            if (piece != null && !string.IsNullOrEmpty(ImprovedBuildHudConfig.CanBuildAmountFormat.Value))
+            if (piece.m_resources.Length == 0)
             {
-                var displayName = Localization.instance.Localize(piece.m_name);
-                if (piece.m_resources.Length == 0)
-                {
-                    return;
-                }
-
-                var fewestPossible = int.MaxValue;
-                foreach (var requirement in piece.m_resources)
-                {
-                    var currentAmount = ImprovedBuildHud.GetAvailableItems(requirement.m_resItem.m_itemData.m_shared.m_name);
-                    var canMake = currentAmount / requirement.m_amount;
-                    if (canMake < fewestPossible)
-                    {
-                        fewestPossible = canMake;
-                    }
-                }
-
-                var canBuildDisplay = string.Format(ImprovedBuildHudConfig.CanBuildAmountFormat.Value, fewestPossible);
-                if (!string.IsNullOrEmpty(ImprovedBuildHudConfig.CanBuildAmountColor.Value))
-                {
-                    canBuildDisplay = $"<color={ImprovedBuildHudConfig.CanBuildAmountColor.Value}>{canBuildDisplay}</color>";
-                }
-
-                ___m_buildSelection.text = $"{displayName} {canBuildDisplay}";
+                return;
             }
+
+            var player = Player.m_localPlayer;
+
+            var fewestPossible = int.MaxValue;
+            foreach (var requirement in piece.m_resources)
+            {
+                var currentAmount = player.GetInventory().CountItems(requirement.m_resItem.m_itemData.m_shared.m_name);
+                var canMake = currentAmount / requirement.m_amount;
+                if (canMake < fewestPossible)
+                {
+                    fewestPossible = canMake;
+                }
+            }
+
+            var canBuildDisplay = string.Format(ImprovedBuildHudConfig.CanBuildAmountFormat.Value, fewestPossible);
+            if (!string.IsNullOrEmpty(ImprovedBuildHudConfig.CanBuildAmountColor.Value))
+            {
+                canBuildDisplay = $"<color={ImprovedBuildHudConfig.CanBuildAmountColor.Value}>{canBuildDisplay}</color>";
+            }
+
+            ___m_buildSelection.text = $"{___m_buildSelection.text} {canBuildDisplay}";
         }
     }
 }
