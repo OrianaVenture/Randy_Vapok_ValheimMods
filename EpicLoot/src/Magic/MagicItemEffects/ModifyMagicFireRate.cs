ecb5369b252e55c6b9edc3fdbc0e57ce65d043ca
warp <73610029+jneb802@users.noreply.github.com>
2025-08-18 17:45:51 -0700

    Bugfix: Fix null reference on non-player characters

--- EpicLoot/src/Magic/MagicItemEffects/ModifyMagicFireRate.cs
+++ EpicLoot/src/Magic/MagicItemEffects/ModifyMagicFireRate.cs
@@ -2,7 +2,7 @@ using HarmonyLib;
 
 namespace EpicLoot.MagicItemEffects;
 
-public class ModifyFireRate
+public class ModifyMagicFireRate
 {
     [HarmonyPatch(typeof(Attack), nameof(Attack.UpdateProjectile))]
     public static class ModifyFireRate_Attack_UpdateProjectile_Patch
@@ -11,6 +11,7 @@ public class ModifyFireRate
         
         public static void Prefix(Attack __instance)
         {
+            if (__instance.m_character is not Player) return;
             Player player = __instance.m_character as Player;
             if (player.HasActiveMagicEffect(MagicEffectType.ModifyMagicFireRate, out float effect, 0.01f)) {
                 originalBurstValue = __instance.m_burstInterval;
@@ -20,6 +21,7 @@ public class ModifyFireRate
         
         public static void Postfix(Attack __instance)
         {
+            if (__instance.m_character is not Player) return;
             Player player = __instance.m_character as Player;
             if (player.HasActiveMagicEffect(MagicEffectType.ModifyMagicFireRate, out float _)) {
                 __instance.m_burstInterval = originalBurstValue;
