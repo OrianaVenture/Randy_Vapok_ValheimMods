d2f8990247b722dbcb45453e965475a0748d13c6
OrianaVenture <107574865+OrianaVenture@users.noreply.github.com>
2026-01-03 14:33:37 -0600

    Add missing EpicAssets from previous PR.

--- EpicLoot/src/General/EpicAssets.cs
+++ EpicLoot/src/General/EpicAssets.cs
@@ -35,6 +35,25 @@ public sealed class EpicAssets
     public GameObject AbilityBar;
     public GameObject WelcomMessagePrefab;
 
+    public SE_Stats BulwarkStatusEffect;
+    public SE_Stats BerserkerStatusEffect;
+    public SE_Stats UndyingStatusEffect;
+
+    public GameObject BulwarkMagicShieldVFX;
+    public GameObject BulwarkMagicShieldSFX;
+
+    public GameObject BerserkerVFX;
+    public GameObject BerserkerSFX;
+
+    public GameObject UndyingVFX;
+    public GameObject UndyingSFX;
+
+    public const string Undying_SE_Name = "UndyingStatusEffect";
+    public const string Bulwark_SE_Name = "BulwarkStatusEffect";
+    public const string Berserker_SE_Name = "BerserkerStatusEffect";
+
+    public const string ExplosiveArrow = "EL_ExplosiveArrow";
+
     public const string DummyName = "EL_DummyPrefab";
     public static GameObject DummyPrefab() => PrefabManager.Instance.GetPrefab(DummyName);
 }
