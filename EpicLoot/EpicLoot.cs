825c7cc6e2c541f587a6b5f1867e2df08c405074
redseiko <redseiko@users.noreply.github.com>
2026-01-03 13:30:14 -0700

    Extract `EpicAssets` class into its own class file and delete unused `PieceDef` class. (#78)
    * Suppress `IDE0130: Namespace does not match folder structure` diagnostic.
    * Extract `EpicAssets` class into its own class file and delete unused `PieceDef` class.
    ---------
    
    Co-authored-by: OrianaVenture <107574865+OrianaVenture@users.noreply.github.com>

--- EpicLoot/EpicLoot.cs
+++ EpicLoot/EpicLoot.cs
@@ -1,4 +1,4 @@
-﻿using BepInEx;
+using BepInEx;
 using Common;
 using EpicLoot.Adventure;
 using EpicLoot.Config;
@@ -25,67 +25,6 @@ using Object = UnityEngine.Object;
 
 namespace EpicLoot
 {
-    public sealed class EpicAssets
-    {
-        public AssetBundle AssetBundle;
-        public static Dictionary<string, Object> AssetCache = new Dictionary<string, Object>();
-
-        public Sprite EquippedSprite;
-        public Sprite AugaEquippedSprite;
-        public Sprite GenericSetItemSprite;
-        public Sprite AugaSetItemSprite;
-        public Sprite GenericItemBgSprite;
-        public Sprite AugaItemBgSprite;
-        public Sprite EnchantmentSparkle;
-        public GameObject[] MagicItemLootBeamPrefabs = new GameObject[5];
-        public readonly Dictionary<string, GameObject[]> CraftingMaterialPrefabs = new Dictionary<string, GameObject[]>();
-        public Sprite SmallButtonEnchantOverlay;
-        public Sprite DodgeBuffSprite;
-        public AudioClip[] MagicItemDropSFX = new AudioClip[5];
-        public AudioClip ItemLoopSFX;
-        public AudioClip AugmentItemSFX;
-        public GameObject MerchantPanel;
-        public Sprite MapIconTreasureMap;
-        public Sprite MapIconBounty;
-        public AudioClip AbandonBountySFX;
-        public AudioClip DoubleJumpSFX;
-        public AudioClip DodgeBuffSFX;
-        public AudioClip OffSetSFX;
-        public GameObject DebugTextPrefab;
-        public GameObject AbilityBar;
-        public GameObject WelcomMessagePrefab;
-
-        public SE_Stats BulwarkStatusEffect;
-        public SE_Stats BerserkerStatusEffect;
-        public SE_Stats UndyingStatusEffect;
-        
-        public GameObject BulwarkMagicShieldVFX;
-        public GameObject BulwarkMagicShieldSFX;
-        
-        public GameObject BerserkerVFX;
-        public GameObject BerserkerSFX;
-
-        public GameObject UndyingVFX;
-        public GameObject UndyingSFX;
-
-        public const string DummyName = "EL_DummyPrefab";
-        public const string Undying_SE_Name = "UndyingStatusEffect";
-        public const string Bulwark_SE_Name = "BulwarkStatusEffect";
-        public const string Berserker_SE_Name = "BerserkerStatusEffect";
-
-        public const string ExplosiveArrow = "EL_ExplosiveArrow";
-
-        public static GameObject DummyPrefab() => PrefabManager.Instance.GetPrefab(DummyName);
-    }
-
-    public sealed class PieceDef
-    {
-        public string Table;
-        public string CraftingStation;
-        public string ExtendStation;
-        public List<RecipeRequirementConfig> Resources = new List<RecipeRequirementConfig>();
-    }
-
     [BepInPlugin(PluginId, DisplayName, Version)]
     [BepInDependency(Jotunn.Main.ModGuid)]
     [BepInDependency("com.ValheimModding.NewtonsoftJsonDetector")]
