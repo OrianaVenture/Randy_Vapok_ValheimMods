4180223aaf441d346fbfa14ef8502b7f8a4ae161
Rusty <107761493+RustyMods@users.noreply.github.com>
2026-01-13 16:23:40 -0500

    again

--- EpicLoot/src/Compendium/MagicEffectTextInfo.cs
+++ EpicLoot/src/Compendium/MagicEffectTextInfo.cs
@@ -48,7 +48,7 @@ public class MagicEffectTextInfo(string topic) : MagicTextInfo(topic)
                 MagicItemEffect effect = entry2.Key;
                 ItemDrop.ItemData item = entry2.Value;
                 content.Add($" <color=#c0c0c0ff>- {MagicItem.GetEffectText(effect, item.GetRarity(), false)} " + 
-                    $"({item.GetDecoratedName()})</color>");            
+                    $"({item.GetDecoratedName()})</color>");
             }
 
             instance.MagicPagesTextArea.Add($"<size={MagicPages.MEDIUM_FONT_SIZE}>" +
